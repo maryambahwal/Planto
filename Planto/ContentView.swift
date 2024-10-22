bcdaad24c2de1354243829adb9142472e1bb06ea
Maryam Bahwal <mrim2m7md@gmail.com>
2024-10-22 09:40:43 +0300

    Initial Commit

---
+++ Planto/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  Planto
+//
+//  Created by Maryam Bahwal on 19/04/1446 AH.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ Planto/PlantoApp.swift
@@ -0,0 +1,17 @@
+//
+//  PlantoApp.swift
+//  Planto
+//
+//  Created by Maryam Bahwal on 19/04/1446 AH.
+//
+
+import SwiftUI
+
+@main
+struct PlantoApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
